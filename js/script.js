6fee1ffaed4e470af0b331ba22cfd135a5d2acce
Natali <C:\Users\Oleksandr\AppData\Roaming\The Bat!>
2022-08-23 17:16:23 +0300

    Deleted setter for salary, suspended isWeekend function

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ function getAge(birthday) {
     let now = new Date();
     return ((Date.parse(now) - Date.parse(birthday)) / msInAYear).toFixed(0);
 }
-
 class Person {
     constructor(firstName, lastName, birthDayDate){
         this.firstName = firstName;
@@ -18,49 +17,42 @@ class Person {
     }
 }
 
-function getCurrentYear() {
-    let currentDate = new Date();
-    return currentDate.getFullYear();
-}
+// function getCurrentYear() {
+//     let currentDate = new Date();
+//     return currentDate.getFullYear();
+// }
+// function isWeekendThisYear(birthday) {
+//     let thisYearBDayInMs = birthday.setFullYear(getCurrentYear);
+//     let thisYearBDay = new Date(thisYearBDayInMs);
+//     return (thisYearBDay.getDay() === 0 || thisYearBDay.getDay() === 6);
+// }
 
-function isWeekendThisYear(birthday) {
-    let thisYearBDayInMs = birthday.setFullYear(getCurrentYear);
-    let thisYearBDay = new Date(thisYearBDayInMs);
-    return (thisYearBDay.getDay() === 0 || thisYearBDay.getDay() === 6);
-}
-
-
+function isWeekend (){}
 
 class Employee extends Person {
-    constructor(firstName, lastName, birthDayDate, jobPosition) {
-        super(firstName, lastName, birthDayDate);
-        this.jobPosition = jobPosition;
-    }
-
     #salary = 0;
-    set salary(salary){
-        return this.#salary = salary;
+    constructor(firstName, lastName, birthDayDate, salary, jobPosition) {
+        super(firstName, lastName, birthDayDate);
+        this.#salary = salary;
+        this.jobPosition = jobPosition;
     }
     get salary(){
         return this.#salary;
     }
-
     getYearSalary(){
         return this.#salary * 12;
     }
     celebrate(){
-        if (isWeekendThisYear(this.birthDayDate)) {
-            console.log(`Happy Birthday, let’s celebrate`);
-            return;
-        }
-        console.log(`Happy Birthday, but I need to work`);
+        // if (!isWeekendThisYear(this.birthDayDate)) {
+        //     console.log(`Happy Birthday, but I need to work`);
+        //     return;
+        // }
+        console.log(`Happy Birthday, let’s celebrate`);
     }
 }
 
 
-
-
 let person = new Person ('Jack', 'Lockwood', '1978-07-12');
 person.celebrate()
-let employee = new Employee ('Kate', 'Fergusson', '1998-08-19', 'designer');
+let employee = new Employee ('Kate', 'Fergusson', '1998-08-19', 20000, 'designer');
 employee.celebrate()
