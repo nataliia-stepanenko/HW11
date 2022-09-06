eed845c07485390b17a2fd6df15150a118e7a3dd
nataliia-stepanenko <natalyss.contact@gmail.com>
2022-09-06 22:24:20 +0300

    Made clear functions getCurrentYear and isWeekend outside the classes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 "use strict"
 
-let msInAYear = 60 * 60 * 24 * 1000 * 365;
+const msInAYear = 60 * 60 * 24 * 1000 * 365;
 function getAge(birthday) {
     let now = new Date();
     return ((Date.parse(now) - Date.parse(birthday)) / msInAYear).toFixed(0);
 }
+function getCurrentYear() {
+    let currentDate = new Date();
+    return currentDate.getFullYear();
+}
+function isWeekend(birthday) {
+    return birthday.getDay() === 0 && birthday.getDay() === 6
+} 
+
+
 class Person {
     constructor(firstName, lastName, birthDayDate){
         this.firstName = firstName;
@@ -16,18 +25,6 @@ class Person {
         console.log(`Happy Birthday, let’s celebrate`);
     }
 }
-
-// function getCurrentYear() {
-//     let currentDate = new Date();
-//     return currentDate.getFullYear();
-// }
-// function isWeekendThisYear(birthday) {
-//     let thisYearBDayInMs = birthday.setFullYear(getCurrentYear);
-//     let thisYearBDay = new Date(thisYearBDayInMs);
-//     return (thisYearBDay.getDay() === 0 || thisYearBDay.getDay() === 6);
-// }
-
-function isWeekend (){}
 
 class Employee extends Person {
     #salary = 0;
@@ -42,17 +39,24 @@ class Employee extends Person {
     getYearSalary(){
         return this.#salary * 12;
     }
-    celebrate(){
-        // if (!isWeekendThisYear(this.birthDayDate)) {
-        //     console.log(`Happy Birthday, but I need to work`);
-        //     return;
-        // }
+    celebrate() {
+        let thisYearBDayInMs = this.birthDayDate.setFullYear(getCurrentYear);
+        let thisYearBDay = new Date(thisYearBDayInMs);
+        if (!isWeekend(thisYearBDay)) {
+            console.log(`Happy Birthday, but I need to work`);
+            return;
+        }
         console.log(`Happy Birthday, let’s celebrate`);
     }
 }
 
 
-let person = new Person ('Jack', 'Lockwood', '1978-07-12');
-person.celebrate()
-let employee = new Employee ('Kate', 'Fergusson', '1998-08-19', 20000, 'designer');
-employee.celebrate()
+
+
+let person = new Person('Jack', 'Lockwood', '1978-07-12');
+console.log(person);
+person.celebrate() // Happy Birthday, let’s celebrate
+
+let employee = new Employee('Kate', 'Fergusson', '1998-08-19', 20000, 'designer');
+console.log(employee);
+employee.celebrate() // Happy Birthday, but I need to work
